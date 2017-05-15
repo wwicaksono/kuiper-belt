489ed56d34b01a9464cb62e8a94e5451b5607fa2
wisnuwicak <kepalakucing2@gmail.com>
2017-05-15 15:36:09 +0700

    try fix controller

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -5,22 +5,49 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot;
 
 class BotController extends Controller
 {
     public function test(Request $request){
-    	$replyToken = $request->input('replyToken');
+        $channelAccess = env('CHANNEL_ACCESS');
+        $channelSecret = env('CHANNEL_SECRET');
+        $bot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-    	$channelAccess = env('CHANNEL_ACCESS');
-    	$channelSecret = env('CHANNEL_SECRET');
-    	
-    	$httpClient = new CurlHTTPClient($channelAccess);
+        $signature = $req->getHeader(HTTPHeader::LINE_SIGNATURE);
+        if (empty($signature)) {
+            return $res->withStatus(400, 'Bad Request');
+        }
 
-    	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+        // Check request with signature and parse request
+        try {
+            $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
+        } catch (InvalidSignatureException $e) {
+            return $res->withStatus(400, 'Invalid signature');
+        } catch (UnknownEventTypeException $e) {
+            return $res->withStatus(400, 'Unknown event type has come');
+        } catch (UnknownMessageTypeException $e) {
+            return $res->withStatus(400, 'Unknown message type has come');
+        } catch (InvalidEventRequestException $e) {
+            return $res->withStatus(400, "Invalid event request");
+        }
 
-    	$textMessageBuilder = new TextMessageBuilder('hello');
-    	$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        foreach ($events as $event) {
+            if (!($event instanceof MessageEvent)) {
+                $logger->info('Non message event has come');
+                continue;
+            }
 
-    	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+            if (!($event instanceof TextMessage)) {
+                $logger->info('Non text message has come');
+                continue;
+            }
+
+            $replyText = $event->getText();
+            $resp = $bot->replyText($event->getReplyToken(), $replyText);
+        }
+
+        $res->write('OK');
+        return $res;
     }
 }
